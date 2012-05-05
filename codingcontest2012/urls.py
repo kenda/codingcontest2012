ea495cd7b0fa87e4235fc425945b4f9475d50500
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2012-05-05 10:52:56 +0200

    [Mod] URLconf modularisiert

--- codingcontest2012/urls.py
+++ codingcontest2012/urls.py
@@ -1,23 +1,31 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 from tastypie.api import Api
+
 from books.api.resources import AutorResource, BuchResource, GenreResource, SammlungResource, TagResource, VerlagResource
 
+# Registrierung der einzelnen REST-Resourcen
 v1_api = Api(api_name='v1')
 v1_api.register(AutorResource())
 v1_api.register(BuchResource())
 v1_api.register(GenreResource())
+v1_api.register(SammlungResource())
 v1_api.register(TagResource())
 v1_api.register(VerlagResource())
-v1_api.register(SammlungResource())
 
 urlpatterns = patterns('',
+    # Admin-Interface
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    # URL der REST-Schnittstelle
     (r'^api/', include(v1_api.urls)),
+
+    # Books-App einbinden
+    url(r'^', include('books.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
