a404ab38e614f9f0a69c4629ab65194230a146f5
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2012-05-06 23:03:43 +0200

    [Mod] Beispiel-DB gefüllt

--- books/forms.py
+++ books/forms.py
@@ -12,16 +12,16 @@ class BuchForm(ModelForm):
     Formular für das Anlegen und Ändern von Buch-Instanzen.
     """
     titel = CharField(widget=TextInput(attrs={'size':'50'}))
-    bewertung = ChoiceField(widget=Select, choices=(
+    bewertung = ChoiceField(widget=Select, required=False, choices=(
             (1, "1"),
             (2, "2"),
             (3, "3"),
             (4, "4"),
             (5, "5")
             ))
-    veroeffentlichung = DateField(widget=SelectDateWidget())
-    preis = CharField(widget=TextInput(attrs={'size':'10'}))
-    lesezeichen = IntegerField(widget=TextInput(attrs={'size':'10'}))
+    veroeffentlichung = DateField(widget=SelectDateWidget(), required=False)
+    preis = CharField(widget=TextInput(attrs={'size':'10'}), required=False)
+    lesezeichen = IntegerField(widget=TextInput(attrs={'size':'10'}), required=False)
     
     class Meta:
         model = Buch
